445f53a029df0d620eda6227b11e65a5e7a8fcbb
Muhammad Subhan Shabeer <124787852+mu-subhan@users.noreply.github.com>
2024-06-08 07:59:03 +0500

    Time: 0 ms (100.00%) | Memory: 7.5 MB (26.99%) - LeetSync

--- 742-to-lower-case/to-lower-case.cpp
+++ 742-to-lower-case/to-lower-case.cpp
@@ -3,7 +3,7 @@ public:
     string toLowerCase(string s) {
      vector<char> n(s.size());
      for(int i=0;i<s.size();i++){
-        n[i] = std::tolower(s[i]);
+        n[i] =tolower(s[i]);
      }
      return string(n.begin(), n.end());      
     }
