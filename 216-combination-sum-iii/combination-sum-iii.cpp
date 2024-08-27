1025725ddf14093bbf693635b43d15140cbf097e
Muhammad Subhan Shabeer <124787852+mu-subhan@users.noreply.github.com>
2024-08-27 23:41:35 +0500

    Time: 3 ms (49.73%) | Memory: 8.3 MB (49.51%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
-    std::vector<std::vector<int>> combinationSum3(int k, int n) {
-        std::vector<std::vector<int>> result;
-        std::vector<int> combination;
-        std::vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        backtrack(nums, k, n, 0, combination, result);
-        return result;
-    }
 
-private:
-    void backtrack(const std::vector<int>& nums, int k, int n, int start, 
-                   std::vector<int>& combination, std::vector<std::vector<int>>& result) {
-        if (combination.size() == k && n == 0) {
-            result.push_back(combination);
+    void backTrackSolve(int sum, int size, vector<vector<int>>& nums, int indx, vector<int>& path, int currSum, vector<vector<int>>& res) 
+    {
+        if (path.size() == size && currSum == sum) 
+        {
+            res.push_back(path);
             return;
         }
-        for (int i = start; i < nums.size(); ++i) {
-            if (n < nums[i]) continue; // Skip if current number is greater than remaining sum
-            combination.push_back(nums[i]);
-            backtrack(nums, k, n - nums[i], i + 1, combination, result);
-            combination.pop_back(); // Remove last element to try the next candidate
+        
+        if (indx >= nums.size() || path.size() > size || currSum > sum)
+            return;
+    
+        // Choose to not pick this no.
+        backTrackSolve(sum, size, nums, indx + 1, path, currSum, res);
+
+        // Choose to pick this no, so also add this number to the combination pair, as currSum + nums[index][0]
+        path.push_back(nums[indx][0]);
+        backTrackSolve(sum, size, nums, indx + 1, path, currSum + nums[indx][0], res);
+        path.pop_back(); // Backtrack
+
+    }
+
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> nums;
+        vector<vector<int>> res;
+        vector<int> path;
+        for (int i = 1; i <= 9; i++) {
+            nums.push_back({i});
         }
+
+        backTrackSolve(n, k, nums, 0, path, 0, res);
+        return res;
     }
 };
