a554018b39e960eadd3188c4b94fc76c078b9bd4
Muhammad Subhan Shabeer <124787852+mu-subhan@users.noreply.github.com>
2025-06-12 14:57:59 +0500

    Time: 0 ms (100.00%) | Memory: 24 MB (9.89%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,11 +1,25 @@
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//         for (int i=0;i<nums.size()-1;i++){
+//             for(int j=i+1;j<nums.size();j++){
+//                 if(nums[i]==0 && nums[j]!=0) swap(nums[i],nums[j]);
+//             }
+//         }
+//     }
+// };
+
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        for(int i=0;i<nums.size()-1;i++){
-           for(int j=i+1;j<nums.size();j++){
-           if(nums[i]==0) swap(nums[i],nums[j]);
-           }
+        int nonZeroPos = 0; 
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[nonZeroPos++] = nums[i];
+            }
         }
-
+        while (nonZeroPos < nums.size()) {
+            nums[nonZeroPos++] = 0;
+        }
     }
 };
