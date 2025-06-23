0a0957f252d6f47feefbb0cd3754f563a18fea01
Muhammad Subhan Shabeer <124787852+mu-subhan@users.noreply.github.com>
2025-06-24 00:39:05 +0500

    Time: 0 ms (100.00%) | Memory: 10.7 MB (74.54%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
-        int finalanser=0;
-        int currsum=0;
-        for(int i=0;i<gain.size();i++){
-       currsum += gain[i];
-       finalanser=max(currsum,finalanser);
+        int currentAltitude = 0;
+        int highestAltitude = 0;
+        for (int i = 0; i < gain.size(); i++) {
+            currentAltitude += gain[i];
+            if (currentAltitude > highestAltitude) {
+                highestAltitude = currentAltitude;
+            }
         }
-        return finalanser;
+        return highestAltitude;
     }
 };
