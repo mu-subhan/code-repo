7a7c37902e5501274d167e8078427823cac4ffa5
Muhammad Subhan Shabeer <124787852+mu-subhan@users.noreply.github.com>
2025-10-14 18:06:30 +0500

    Time: 0 ms (100.00%) | Memory: 10.3 MB (59.03%) - LeetSync

---
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+       
+       int i=0,j=s.size()-1;
+       while(i<j){
+       while (i < j) {
+    
+    while (i < j && (s[i] != 'a' && s[i] != 'e' && s[i] != 'i' && s[i] != 'o' && s[i] != 'u' && 
+                     s[i] != 'A' && s[i] != 'E' && s[i] != 'I' && s[i] != 'O' && s[i] != 'U')) {
+        i++;
+    }
+    
+    
+    while (i < j && (s[j] != 'a' && s[j] != 'e' && s[j] != 'i' && s[j] != 'o' && s[j] != 'u' && 
+                     s[j] != 'A' && s[j] != 'E' && s[j] != 'I' && s[j] != 'O' && s[j] != 'U')) {
+        j--;
+    }
+
+
+    if (i < j) {
+        char res = s[i];
+        s[i] = s[j];
+        s[j] = res;
+        i++;
+        j--;
+    }
+       }
+       } 
+       return s;
+    }
+};
